cb82da91f338daa0ddbb6f0d0bbdf6c322a120c7
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-21 09:21:44 -0300

    add  Component table #03

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -1,11 +1,19 @@
 import { JSXElementConstructor, Key, ReactElement, ReactFragment, ReactPortal } from "react";
 import Cliente from "../core/Cliente";
+import { IconeLixo } from "./Icones";
+import { IconeEdicao } from "./Icones";
 
 interface TabelaProps {
     clientes: Cliente[]
+    clienteSelecionado?: (cliente: Cliente) => void
+    clienteExcluido?: (cliente: Cliente) => void
 }
 
 export default function Tabela(props: any) {
+
+    const exibirAcoes = props.clienteExcluido || props.clienteSelecionado
+
+
 
     function renderizarCabecalho() {
         return (
@@ -21,18 +29,52 @@ export default function Tabela(props: any) {
     function renderizarDados() {
         return props.clientes?.map((cliente: { id: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; nome: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; idade: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }, id: any) => {
             return (
-                <tr key={cliente.id}>
-                    <td>{cliente.id}</td>
-                    <td>{cliente.nome}</td>
-                    <td>{cliente.idade}</td>
-                </tr>
+                <tr key={cliente.id}
+                className={`${id % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}>
+                <td className="text-left p-4">{cliente.id}</td>
+                <td className="text-left p-4">{cliente.nome}</td>
+                <td className="text-left p-4">{cliente.idade}</td>
+            </tr>
+                
             )
         })
     }
 
+    function renderizarAcoes(cliente: Cliente) {
+        return (
+            <td className="flex justify-center">
+                {props.clienteSelecionado ? (
+
+                    <button onClick={() => props.clienteSelecionado?.(cliente)} className={`
+                    flex justify-center items-center
+                    text-green-600 rounded-full p-2 m-1
+                    hover:bg-purple-50
+                    `}>
+                        {IconeEdicao}
+                    </button>
+
+                ) : false }
+
+                {props.clienteExcluido ? (
+
+                <button onClick={() => props.clienteExcluido?.(cliente)} className={`
+                flex justify-center items-center
+                text-red-500 rounded-full p-2 m-1
+                hover:bg-purple-50
+                `}>{IconeLixo}</button>
+
+                ) : false }
+            </td>
+        )
+    }
+
     return (
-        <table>
-            <thead>
+
+            <table className="w-full rounded-xl overflow-hidden">
+            <thead className={`
+            text-gray-100
+            bg-gradient-to-r from-purple-500 to-purple-800
+            `}>
                 {renderizarCabecalho()}
             </thead>
             <tbody>
